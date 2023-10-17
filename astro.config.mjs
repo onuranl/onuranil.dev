0964dad9a6c0a9bdb5346c78353bcaf24ec8c939
onuranl <onuro99cent@gmail.com>
2023-10-17 12:59:30 +0300

    alhamdulillah

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fdemir.dev",
+  site: "https://onuranil.dev",
 
   integrations: [solidJs(), robotsTxt()],
 });
